8e8fa0fc3904c694cdca2add6b8c535cba1edc7b
Patel Anvi Alpesh <101973868+AnviPatel24@users.noreply.github.com>
2023-08-02 13:14:24 +0530

    Add files via upload

--- prc3.py
+++ prc3.py
@@ -15,8 +15,20 @@ width=int(input("Enter Width: "))
 print("Inputted Height is: ",height)
 print("Inputted Width is: ",width)
 
-for i in range(height-2,width+2):
-    for j in range(height-2,width+2):
-        
+hid,wid=image3.shape
+print("Height is: ",hid)
+print("Width is: ",wid)
+for i in range(height-2,height+2):
+     for j in range(width-2,width+2):
+         #print("image[",i,"]","[",j,"]","= ",image3[i][j])
+         if(i==height and j==width):
+             print("Left",image3[i][j-1])
+             print("Top",image3[i-1][j])
+             print("Right",image3[i][j+1])
+             print("Bottom",image3[i+1][j])
+             print("Diagonal left up",image3[i-1][j-1])
+             print("Diagonal rightt up",image3[i-1][j+1])
+             print("Diagonal left down",image3[i+1][j-1])
+             print("Diagonal right down",image3[i+1][j+1])
 cv2.waitKey(0)
 cv2.destroyAllWindows()
